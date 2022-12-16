9fdf50e44bd2402d648bb105464f890754fc8078
Kirill G <kirillgalenko@gmail.com>
2022-12-16 14:55:05 +0200

    first commit

---
+++ src/components/ui/buttons/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  
+  export let style = "default";
+  
+  let classList = 'inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium  shadow-sm  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2';
+
+  if(style === "outline") {
+    classList += ' bg-white text-gray-500 border-gray-300 hover:bg-gray-50';
+  } else if(style === "secondary") {
+    classList += ' bg-gray-500 text-white hover:bg-gray-700';
+  } else {
+    classList += ' bg-indigo-500 text-white hover:bg-indigo-700';
+  }
+
+</script>
+<button class={classList} on:click={() => dispatch('click')}>
+  <slot />
+</button>
---
+++ src/routes/exercises/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Button from '../../components/ui/buttons/Button.svelte';
+
+  let newExercise = '';
+
+  let exercises = [
+      { slug: 'squat', title: 'Squat' },
+      { slug: 'bench-press', title: 'Bench press' },
+      { slug: 'deadlift', title: 'Deadlift' },
+      { slug: 'row', title: 'Row' },
+      { slug: 'push-up', title: 'Push-up' }
+    ];
+
+  const addExercise = () => {
+    if(!newExercise) return;
+    exercises = [...exercises, { slug: newExercise.toLowerCase()
+                      .trim()
+                      .replace(/[^\w\s-]/g, '')
+                      .replace(/[\s_-]+/g, '-')
+                      .replace(/^-+|-+$/g, ''), title: newExercise }];
+    newExercise = '';
+  }
+</script>
+
+<h1>Exercises page is here!</h1>
+<ul>
+  {#each exercises as exercise}
+    <li><a href="/exercises/{exercise.slug}">{exercise.title}</a></li>
+  {/each}
+</ul>
+<form>
+  <input 
+    bind:value={newExercise}
+    type="text" 
+    placeholder="New exercise name" 
+    class="border-gray-300 border-solid border rounded-md p-2 text-sm" 
+  />
+  <Button on:click={addExercise}>Add Exercise</Button>
+  <Button style="secondary">Add Exercise</Button>
+  <Button style="outline">Add Exercise</Button>
+</form>
